ec2332c48a2ba20d3f5c08bb150d8c7396117ddd
Felix Weiglhofer <9267733+fweig@users.noreply.github.com>
2025-03-24 21:37:28 +0100

    Initial commit.

---
+++ mirror.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# TODO: handle missing arguments
+Source=$1
+TargetDir=$2
+
+TmpDir="/tmp/vim-packages"
+
+
+rm -rf $TmpDir
+
+git clone --quiet "$Source" "$TmpDir"
+
+mkdir -p "$TargetDir"
+
+cp -r "$TmpDir"/* "$TargetDir/"
+
+echo "Source=$Source" > "$TargetDir/.MANIFEST"
+
+Commit=$(cd $TmpDir && git rev-parse HEAD)
+echo "Commit=$Commit" >> "$TargetDir/.MANIFEST"
+
